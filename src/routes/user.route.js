2877497bcbdf42dcbb090d808ffabf3fa4d882a8
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-08-13 22:02:03 +0530

    fix matching id issue'

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -17,9 +17,16 @@ router.get("/user/connections", authUser, async (req, res) => {
                 senderId: loggedInUser._id,
             }
             ]
-        }).populate({ path: "senderId", select: "-password -createdAt -updatedAt -__v -email" }).exec()
+        }).populate({ path: "senderId receiverId", select: "-password -createdAt -updatedAt -__v -email" }).exec()
 
-        const data = connections.map((user) => user.senderId)
+        const data = connections.map((user) => {
+            if (user.senderId._id.toString() === loggedInUser._id.toString()) {
+                return user.receiverId
+            } else {
+                return user.senderId
+
+            }
+        })
 
 
         return res.status(200).json({ data })
@@ -28,6 +35,7 @@ router.get("/user/connections", authUser, async (req, res) => {
         return res.status(500).json({ message: error.message || "Something went wrong" })
     }
 })
+
 router.get("/user/requests/received", authUser, async (req, res) => {
     try {
         const loggedInUser = req.user;
@@ -43,5 +51,7 @@ router.get("/user/requests/received", authUser, async (req, res) => {
         return res.status(500).json({ message: error.message || "Something went wrong" })
     }
 })
+
+
 
 module.exports = router

df8834e3a70c27a5d1a7ab0999d8473cf364932d
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-08-15 21:03:37 +0530

    feed api

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { authUser } = require("../middleware/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const { User } = require("../models/user");
 const router = express.Router();
 
 
@@ -52,6 +53,45 @@ router.get("/user/requests/received", authUser, async (req, res) => {
     }
 })
 
+
+router.get("/user/feed", authUser, async (req, res) => {
+    try {
+        // TODO :
+        // 1.) User should see all the user card
+        //     - he should not see his own card 
+        //     - he should not see his connections 
+        //     - he should not see ignored people 
+        //     - he should not see already sent the connection request 
+        const loggedInUser = req.user
+
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or: [
+                { senderId: loggedInUser._id },
+                { receiverId: loggedInUser._id },
+            ]
+        }).select("senderId receiverId");
+
+        const hideUserFromFeed = new Set();
+
+        connectionRequest.forEach(request => {
+            hideUserFromFeed.add(request.senderId.toString())
+            hideUserFromFeed.add(request.receiverId.toString())
+        })
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUserFromFeed) } },
+                { _id: { $ne: loggedInUser._id } },
+            ]
+        }).select("-password -email")
+
+        return res.status(200).json({ data: users })
+
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message || "Something went wrong" })
+    }
+})
 
 
 module.exports = router

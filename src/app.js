cd926bfab2d56c34956f18ba6d1df08315f90579
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-08-16 12:57:00 +0530

    setup cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors")
+
 const { connectToDB } = require("./config/database");
 const cookieParser = require("cookie-parser")
 const authRouter = require("./routes/auth.route");
@@ -8,11 +10,14 @@ const profileRouter = require("./routes/profile.route");
 const userRouter = require("./routes/user.route");
 
 
+
 dotenv.config();
 
 
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }))
 app.use(express.json())
 app.use(cookieParser())
 

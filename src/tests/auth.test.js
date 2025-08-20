185f13173660ed0d5bf07611eab231ad9ce4521e
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-08-20 21:34:23 +0530

    sesclient setup

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -16,7 +16,7 @@ const app = require('../app');
 
 describe('GET /profile', () => {
   it('should return authenticated user info', async () => {
-    const res = await request(app).get('/api/profile');
+    const res = await request(app).get('/profile');
 
     expect(res.statusCode).toBe(200);
     expect(res.body).toMatchObject({

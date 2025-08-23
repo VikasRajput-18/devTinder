9f7543313195789d812fe9734f62f9ae1f1d5525
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-08-23 10:49:09 +0530

    chat room created and message feature

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,17 @@
 const { Server } = require("socket.io");
+const crypto = require("crypto")
+
+
+const getSecretRoomId = (userId, targetUserId) => {
+    const roomId = [userId, targetUserId].sort().join("_")
+    return crypto.createHash("sha256").update(roomId).digest("hex")
+}
 
 
 const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173/",
+            origin: "http://localhost:5173",
             credentials: true
         }
     })
@@ -12,8 +19,16 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
         console.log("A user connected:", socket.id);
 
-        socket.on("joinChat", () => { })
-        socket.on("sendMessage", () => { })
+        socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+            const hashedRoomId = getSecretRoomId(userId, targetUserId)
+            socket.join(hashedRoomId)
+        })
+        socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+            const hashedRoomId = getSecretRoomId(userId, targetUserId)
+            io.to(hashedRoomId).emit("messageReceived", {
+                firstName, newMessage: text, userId
+            })
+        })
 
         socket.on("disconnect", () => {
             console.log("User disconnected:", socket.id);
